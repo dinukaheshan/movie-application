cbc2a2f3e02da84ee5f36c00721f28318a97f8bc
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-09 08:49:43 +0530

    Implement getAllMovies endpoint in MovieController

---
+++ src/main/java/dev/dinuka/movieapi/MovieRepository.java
@@ -0,0 +1,11 @@
+package dev.dinuka.movieapi;
+
+import org.springframework.stereotype.Repository;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+
+
+}
