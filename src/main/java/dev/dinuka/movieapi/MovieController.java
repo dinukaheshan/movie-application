625090d1337a1eea9aebfea19cc9dc6ff1b88bde
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-10 21:46:16 +0530

    fixing connection string issue

--- src/main/java/dev/dinuka/movieapi/MovieController.java
+++ src/main/java/dev/dinuka/movieapi/MovieController.java
@@ -1,5 +1,4 @@
 package dev.dinuka.movieapi;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
