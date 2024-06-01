414d4b43bb397fcd6a9c08e9eaa568dfa6fa573b
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-01 13:30:58 +0530

    reviews

--- frontend/movie-client/src/App.js
+++ frontend/movie-client/src/App.js
@@ -4,10 +4,15 @@ import Layout from "./components/Layout";
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
+import NotFound from "./components/notFound/NotFound";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -20,6 +25,18 @@ function App() {
     }
   };
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await axios.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     getMovies();
   }, []);
@@ -28,8 +45,11 @@ function App() {
       <div className="App">
         <Header/>
         <Routes>
-          <Route path="/*" element={<Layout />}>
-            <Route index element={<Home movies={movies} />} />
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
           </Route>
         </Routes>
       </div>
